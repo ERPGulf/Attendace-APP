76bed1206a08b03d6b2ac5819ae8599941d4283c
bazimR <rishal@htsqatar.com>
2023-08-11 23:27:12 +0530

    app js fix

--- App.js
+++ App.js
@@ -2,61 +2,22 @@ import { Provider } from 'react-redux';
 import { store } from './redux/Store';
 import { StatusBar } from 'expo-status-bar';
 import Navigator from './navigation/navigator';
-import Toast, { BaseToast, ErrorToast } from "react-native-toast-message";
+import Toast from "react-native-toast-message";
+import { Suspense } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 export default function App() {
-  const toastConfig = {
-    /*
-      Overwrite 'success' type,
-      by modifying the existing `BaseToast` component
-    */
-    success: (props) => (
-      <BaseToast
-        {...props}
-        style={{}}
-        contentContainerStyle={{ paddingHorizontal: 15 }}
-        text1Style={{
-          fontSize: 24,
-          fontWeight: '400',
-          textAlign: 'center'
-
-        }}
-      />
-    ),
-    /*
-      Overwrite 'error' type,
-      by modifying the existing `ErrorToast` component
-    */
-    error: (props) => (
-      <ErrorToast
-        {...props}
-        text1Style={{
-          fontSize: 17
-        }}
-        text2Style={{
-          fontSize: 15
-        }}
-      />
-    ),
-    /*
-      Or create a completely new type - `tomatoToast`,
-      building the layout from scratch.
-  
-      I can consume any custom `props` I want.
-      They will be passed when calling the `show` method (see below)
-    */
-    tomatoToast: ({ text1, props }) => (
-      <View style={{ height: 60, width: '100%', backgroundColor: 'tomato' }}>
-        <Text>{text1}</Text>
-        <Text>{props.uuid}</Text>
-      </View>
-    )
-  };
 
   return (
     <Provider store={store}>
-      <Navigator />
-      <StatusBar style='auto' />
-      <Toast />
+      <Suspense fallback={
+        <View className="flex-1 justify-center items-center">
+          <ActivityIndicator size={'large'} />
+        </View>
+      }>
+        <Navigator />
+        <StatusBar style='auto' />
+        <Toast />
+      </Suspense>
     </Provider>
   );
 }
