a76d0a4d633673e6e2f341a1f3dcd2f09cc52ad0
bazimR <rishal@htsqatar.com>
2023-08-20 01:16:06 +0530

    caching added

--- App.js
+++ App.js
@@ -4,8 +4,32 @@ import { StatusBar } from 'expo-status-bar';
 import Navigator from './navigation/navigator';
 import Toast from "react-native-toast-message";
 import { PersistGate } from 'redux-persist/integration/react';
-export default function App() {
+import { useState } from 'react';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import Ionicons from '@expo/vector-icons/Ionicons'
 
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
+export default function App() {
+  const [appReady, setAppReady] = useState(false)
+
+  useEffect(() => {
+    const loadResourcesAndDataAsync = async () => {
+      try {
+        SplashScreen.preventAutoHideAsync()
+        const IconAssets = cacheFonts([Ionicons.font])
+        await Promise.all([...IconAssets])
+      } catch (error) {
+        console.warn(error)
+      } finally {
+        setAppReady(true)
+        SplashScreen.hideAsync()
+      }
+    }
+  }, [])
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
