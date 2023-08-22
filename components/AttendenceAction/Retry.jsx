fd3c3a6e4375a41ae42dda241e491a9badb79441
bazimR <rishal@htsqatar.com>
2023-08-22 17:58:11 +0530

    code split

---
+++ components/AttendenceAction/Retry.jsx
@@ -0,0 +1,40 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { COLORS } from "../../constants";
+
+const Retry = ({navigation,retry}) => {
+  return (
+    <View
+      style={{}}
+      className="h-screen absolute bottom-0 w-screen items-center  bg-black/70  justify-center z-50"
+    >
+      <Text className="text-lg mb-1 font-normal text-white">
+        Status Retrieval Issue
+      </Text>
+      <View className="w-3/4">
+        <Text className="text-xs  text-center mb-10 font-normal text-gray-200">
+          We apologize, but we're currently experiencing difficulties fetching
+          your status. Please retry or try again later.
+        </Text>
+      </View>
+      <TouchableOpacity
+        style={{ backgroundColor: COLORS.primary }}
+        className="h-12 m-2 flex-row w-3/6 rounded-xl justify-center  space-x-2 items-center"
+        onPress={retry}
+      >
+        <Text className="text-base text-white font-bold">Retry</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{ borderColor: COLORS.primary }}
+        className="h-12 flex-row w-3/6 rounded-xl justify-center border-2 bg-white space-x-2 items-center"
+        onPress={() => navigation.goBack()}
+      >
+        <Text style={{ color: COLORS.primary }} className="text-base font-bold">
+          Go back
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Retry;
--- components/AttendenceAction/index.js
+++ components/AttendenceAction/index.js
@@ -1,5 +1,6 @@
 import WelcomeCard from "./WelcomeCard";
 import FileUpload from "./FileUpload";
+import Retry from "./Retry";
 export {
-    WelcomeCard, FileUpload
+    WelcomeCard, FileUpload,Retry
 }
