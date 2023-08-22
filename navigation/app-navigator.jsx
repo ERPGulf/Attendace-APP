71f8ca95d6321b77e2d8fedfc00bf0a0513b8ca6
bazimR <rishal@htsqatar.com>
2023-08-22 15:19:37 +0530

    clean up

--- navigation/app-navigator.jsx
+++ navigation/app-navigator.jsx
@@ -1,5 +1,4 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import React from "react";
 import { AttendenceAction, AttendenceHistory, Home } from "../screens";
 const Stack = createNativeStackNavigator();
 
