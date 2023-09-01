c94c7bf7c1c9c87cae866819f570c88731ba3839
bazimR <rishal@htsqatar.com>
2023-09-02 00:43:39 +0530

    video

--- screens/AttendanceCamera.jsx
+++ screens/AttendanceCamera.jsx
@@ -9,8 +9,15 @@ import React, { useEffect, useRef, useState } from "react";
 import { Camera } from "expo-camera";
 import Constants from "expo-constants";
 import { Ionicons } from "@expo/vector-icons";
+import { useSelector } from "react-redux";
+import { selectFileid } from "../redux/Slices/UserSlice";
+import { Toast } from "react-native-toast-message/lib/src/Toast";
+import { putUserFile, userFileUpload } from "../api/userApi";
+
 const AttendanceCamera = () => {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasMicroPhonePermission, setHasMicroPhonePermission] = useState(null);
+
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [mode, setMode] = useState("camera");
   const [photo, setPhoto] = useState(null);
@@ -18,7 +25,10 @@ const AttendanceCamera = () => {
   useEffect(() => {
     (async () => {
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microPhonePermission =
+        await Camera.requestMicrophonePermissionsAsync();
       setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMicroPhonePermission(microPhonePermission.status === "granted");
     })();
   }, []);
   const changeCamera = () => {
@@ -28,14 +38,57 @@ const AttendanceCamera = () => {
         : Camera.Constants.Type.back
     );
   };
+  const changeMode = () => {
+    setMode(mode === "camera" ? "video" : "camera");
+  };
   const takePicture = async () => {
     let options = {
-      quality: 1,
+      quality: 0.7,
       exif: false,
       base64: true,
     };
     const newPhoto = await cameraRef.current.takePictureAsync(options);
     setPhoto(newPhoto);
+  };
+  const name = useSelector(selectFileid);
+  const uploadPicture = async () => {
+    console.log(photo.uri);
+    const formData = new FormData();
+    formData.append("file_name", name);
+    formData.append("fieldname", "custom_photo");
+    formData.append("file", {
+      uri: photo.uri,
+      type: "image/jpeg", // Adjust the type based on your image format
+      name: `${name + new Date().toISOString()}.jpg`, // Adjust the filename as needed
+    });
+    formData.append("is_private", "1");
+    formData.append("doctype", "Employee Checkin");
+    formData.append("docname", name);
+    userFileUpload(formData)
+      .then(({ file_url }) => {
+        console.log(file_url);
+        const formData = new FormData();
+        formData.append("custom_image", file_url);
+        putUserFile(formData, name)
+          .then(() => {
+            Toast.show({
+              type: "success",
+              text1: "✅ Photo Uploaded",
+            });
+          })
+          .catch(() => {
+            Toast.show({
+              type: "error",
+              text1: "Photo Upload Failed",
+            });
+          });
+      })
+      .catch(() => {
+        Toast.show({
+          type: "error",
+          text1: "Photo Upload Failed",
+        });
+      });
   };
   if (hasCameraPermission === false) {
     return (
@@ -46,15 +99,44 @@ const AttendanceCamera = () => {
   }
   if (photo) {
     return (
-      <SafeAreaView className="flex-1 items-center justify-center bg-white relative">
-        <Image
-          className="flex-1 w-full h-full"
-          source={{ uri: "data:image/jpg;base64," + photo.base64 }}
-        />
-        <TouchableOpacity onPress={() => console.log("pressed")}>
-          <Text>rest</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
+      <View
+        style={{ paddingVertical: Constants.statusBarHeight }}
+        className="flex-1 items-center justify-center bg-white relative"
+      >
+        <View
+          style={{ width: "100%" }}
+          className="relative px-3 border-b border-black/30"
+        >
+          <View className="flex-row pb-4 pt-2 items-center justify-center relative">
+            <TouchableOpacity
+              className="absolute pb-4 left-0 pt-2"
+              onPress={() => {
+                setPhoto(null);
+              }}
+            >
+              <Text className="text-base font-normal text-red-500">Retake</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="absolute pb-4 right-0 pt-2"
+              onPress={uploadPicture}
+            >
+              <Text className="text-base font-normal text-blue-500">
+                Upload
+              </Text>
+            </TouchableOpacity>
+            <View className="justify-self-center text-center">
+              <Text className="text-xl font-medium">Preview</Text>
+            </View>
+          </View>
+        </View>
+        <View className="w-full flex-1 border-b border-black/30">
+          <Image
+            resizeMode="contain"
+            className="w-full flex-1"
+            source={{ uri: "data:image/jpg;base64," + photo.base64 }}
+          />
+        </View>
+      </View>
     );
   }
   return (
@@ -74,7 +156,7 @@ const AttendanceCamera = () => {
           style={{ width: 80, height: 80 }}
           className="bg-white justify-center items-center rounded-full"
         >
-          <Ionicons name="camera" size={40} color={"darkorange"} />
+          <Ionicons name="ios-camera" size={40} color={"black"} />
         </TouchableOpacity>
         <TouchableOpacity
           onPress={changeCamera}
@@ -82,12 +164,12 @@ const AttendanceCamera = () => {
             width: 80,
             height: 80,
           }}
-          className="justify-center items-center rounded-full left-4 absolute"
+          className="justify-center  items-center rounded-full left-4 absolute"
         >
           <Ionicons name="refresh" size={44} color={"white"} />
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={changeCamera}
+          onPress={changeMode}
           style={{
             width: 80,
             height: 80,
