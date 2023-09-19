aa85bf1fb84e2408cfd6f2b5ca9ea36f15677c44
rishal-ERPGULF <rishal@htsqatar.com>
2023-09-19 23:33:59 +0530

    dom condition render fix

--- navigation/home.tabbar.jsx
+++ navigation/home.tabbar.jsx
@@ -9,20 +9,8 @@ const TabStack = createBottomTabNavigator();
 const HomeTabGroup = () => {
   return (
     <TabStack.Navigator
-      screenOptions={({ route }) => ({
+      screenOptions={() => ({
         tabBarActiveTintColor: COLORS.primary,
-        tabBarIcon: ({ color }) => {
-          if (route.name === "home") {
-           
-            return <AntDesign name={'home'} size={34} color={color} />;
-          }
-          if (route.name === "chat") {
-            return <AntDesign name={'message1'} size={34} color={color} />;
-          }
-          if (route.name === "profile") {
-            return <AntDesign name={'user'} size={34} color={color} />;
-          }
-        },
         headerShown: false,
         tabBarShowLabel: false,
         tabBarStyle: {
@@ -30,17 +18,41 @@ const HomeTabGroup = () => {
           height: 80,
           borderWidth: 0,
           paddingBottom: 0,
-          bottom:10,
+          bottom: 10,
           left: 10,
           right: 10,
           borderRadius: 20,
-          elevation:5,
+          elevation: 5,
         },
       })}
     >
-      <TabStack.Screen name="home" component={Home}/>
-      <TabStack.Screen name="chat" component={Home} />
-      <TabStack.Screen name="profile" component={Profile} />
+      <TabStack.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name={"home"} size={34} color={color} />
+          ),
+        }}
+      />
+      <TabStack.Screen
+        name="chat"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name={"message1"} size={34} color={color} />
+          ),
+        }}
+      />
+      <TabStack.Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name={"user"} size={34} color={color} />
+          ),
+        }}
+      />
     </TabStack.Navigator>
   );
 };
